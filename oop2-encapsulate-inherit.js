06b5b744c834d81f03587d2564e5e743a611edc6
Kevin Ibanez <Kpeibanez@gmail.com>
2020-06-16 21:17:40 -0500

    completed problem 2 of exercise 2

--- oop2-encapsulate-inherit.js
+++ oop2-encapsulate-inherit.js
@@ -159,7 +159,186 @@ shandysReview.info();
 
 /****************************************************************************************************************************************************************************************   
 */
+class Umbrella {
+  constructor(orgName, company1, company2, company3) {
+    this.org = orgName;
+    this.company1 = company1;
+    this.company2 = company2;
+    this.company3 = company3;
+  }
+  orgInfo() {
+    console.log(
+      `${this.org} is the parent organization of ${this.company1}, ${this.company2} and ${this.company3}!`
+    );
+  }
+  orgStocks() {
+    console.log(`${this.org}'s stocks are at 27.78 per stock at this time!`);
+  }
+}
+let generalMotors = new Umbrella(
+  "General Motors",
+  "GMC",
+  "Chevrolet",
+  "Cadillac"
+);
+generalMotors.orgInfo();
 
+class Company extends Umbrella {
+  constructor(orgName, name, type, employees) {
+    super(orgName);
+    this.org = orgName;
+    this.company = name;
+    this.type = type;
+    this.employees = employees;
+  }
+  info() {
+    console.log(`${this.company} is a child organization of ${this.org}`);
+  }
+  carTypes() {
+    console.log(`${this.company} tends to sell ${this.type}!`);
+  }
+}
+
+let gmc = new Company("General Motors", "GMC", "luxury trucks", "164,000");
+gmc.info();
+gmc.carTypes();
+let chevrolet = new Company(
+  "General Motors",
+  "Chevrolet",
+  "everyday day vehicles",
+  "20,000"
+);
+chevrolet.info();
+chevrolet.carTypes();
+let cadillac = new Company(
+  "General Motors",
+  "Cadillac",
+  "luxury cars",
+  "10,000"
+);
+cadillac.info();
+cadillac.carTypes();
+
+class Site extends Company {
+  constructor(name, location) {
+    super(name);
+    this.location = location;
+    this.company = name;
+  }
+  siteLocation() {
+    console.log(`${this.company} is located in ${this.location}`);
+  }
+  avgSummerTemp() {
+    console.log(
+      `The average temp in ${this.location} is 79-59 degrees fahrenheit.`
+    );
+  }
+}
+let gmcSite = new Site("GMC", "Detroit, Michigan");
+gmcSite.siteLocation();
+gmcSite.avgSummerTemp();
+let chevroletSite = new Site("Chevrolet", "Detroit, Michigan");
+chevroletSite.siteLocation();
+chevroletSite.avgSummerTemp();
+let cadillacSite = new Site("Cadillac", "Warren, Michigan");
+cadillacSite.siteLocation();
+cadillacSite.avgSummerTemp();
+class Employee {
+  constructor(name, title, salary, job) {
+    this.name = name;
+    this.title = title;
+    this.salary = salary;
+    this.job = job;
+  }
+  data() {
+    console.log(
+      `${this.name} is a ${this.title} and their salary is ${this.salary}.`
+    );
+  }
+  jobDesc() {
+    console.log(`${this.title} tend to ${this.job}.`);
+  }
+}
+let jesus = new Employee(
+  "Jesus",
+  "CEO",
+  "600,000",
+  "manage the corporation and approves any new models or designs"
+);
+jesus.data();
+jesus.jobDesc();
+let kevin = new Employee(
+  "Kevin",
+  "Software Engineer",
+  "50,000",
+  "keep the website running and manages the database"
+);
+kevin.data();
+kevin.jobDesc();
+let jesel = new Employee(
+  "Jesel",
+  "Secretary",
+  "35,000",
+  "keep the useless stuff out of the CEO's way"
+);
+jesel.data();
+jesel.jobDesc();
+let jose = new Employee(
+  "Jose",
+  "Manager",
+  "75,000",
+  "keep the website running and manages the database"
+);
+jose.data();
+jose.jobDesc();
+let shandy = new Employee(
+  "Shandy",
+  "Financial Officer",
+  "100,000",
+  "keep the money flowing and makes sure the stocks stay positive"
+);
+shandy.data();
+shandy.jobDesc();
+let cindy = new Employee(
+  "Cindy",
+  "HR Personnel",
+  "40,000",
+  "manage the employees and does the hiring process"
+);
+cindy.data();
+cindy.jobDesc();
+let antonio = new Employee(
+  "Antonio",
+  "Fintech Engineer",
+  "100,000",
+  "keep the company's use of technology with finance on top of the game"
+);
+antonio.data();
+antonio.jobDesc();
+let dave = new Employee(
+  "Dave",
+  "Janitor",
+  "25,000",
+  "keep the site clean and spotless"
+);
+dave.data();
+dave.jobDesc();
+let minh = new Employee(
+  "Minh",
+  "Lawyer",
+  "150,000",
+  "handle the lawsuits and keeps them out of the news"
+);
+minh.data();
+minh.jobDesc();
+let joe = new Employee(
+  "Joe",
+  "Marketer",
+  "80,000",
+  "keep the company on the good side of the media and promotes sales"
+);
+joe.data();
+joe.jobDesc();
 /*
 Bonus Exercise:
 
